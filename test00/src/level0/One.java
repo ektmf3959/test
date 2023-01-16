e8d3eca4cfa907efecd8f214fac67927c9693539
YSCOM <tsj02328@naver.com>
2023-01-16 21:58:35 +0900

    2023-01-16 코테 입문 4

--- test00/src/level0/One.java
+++ test00/src/level0/One.java
@@ -2,7 +2,7 @@ package level0;
 
 public class One {
 	
-	
+		/*
 	    public int solution1(int num1, int num2) {
 	        boolean val = (-50000<= num1 && num1<= 50000 && -50000<=num2 && num2 <= 50000);
 	        int answer = -1;
@@ -32,6 +32,14 @@ public class One {
 	        }
 	        return answer;
 	    }
+	    */
+	
+		public int solution4(int num1, int num2) {
+	        int answer = 0;
+	        double value = (double)num1 / num2 * 1000;
+	        answer = (int)value;
+	        return answer;
+		}
 	}
 
 
