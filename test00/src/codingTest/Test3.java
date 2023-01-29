c370cc6a5fd1cfca093a9f52d511552b7afd837f
YSCOM <tsj02328@naver.com>
2023-01-29 23:23:25 +0900

    test

--- test00/src/codingTest/Test3.java
+++ test00/src/codingTest/Test3.java
@@ -1,7 +1,9 @@
 package codingTest;
 
 public class Test3 {
-
+	public static void main(String[] args) {
+		System.out.println("시험");
+	}
 }
 /*
  * https://school.programmers.co.kr/learn/courses/30/lessons/132267
